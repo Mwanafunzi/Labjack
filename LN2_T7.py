de541a32f33eda0476471e87cd7fe6005d971b94
Mwanafunzi <skemp255@gmail.com>
2018-02-17 10:09:53 +0300

    added more error catching, if read fails, doesn't attempt a write

--- LN2_T7.py
+++ LN2_T7.py
@@ -2,6 +2,8 @@
 
 # gets LN2-related information from a specific labjack
 
+# labjack is set to use extended function for average values from AIN0, AIN2 and AIN4.
+# THese AIN's are also set to use signal as positive and adjacent channel as negative.
 
 #uses the "Deep Search" function for searching explicit IP ranges. 
 # The config file lives here:
@@ -21,26 +23,29 @@ from labjack import ljm
 
 import LN2config
 
+
 write_interval = 30*60
-sleep_time = 5
+sleep_time = 25
 connect_wait_time = 10
 
 write_time = 0
 
 read_channels = {
          '1TopPressure':         'AIN2',           # raw pressure input at the AIN. Actually ignored
-         '2Average top P':       'AIN2_EF_READ_A', # average using EF. This will be the the value used
+         '2Average_top_P':       'AIN2_EF_READ_A', # average using EF. This will be the the value used
          '3BottomPressure':      'AIN0',           # raw input at the AIN. Actually ignored
-         '4Average bottom P':    'AIN0_EF_READ_A', # average pressure using EF. This will be the the value used
-         '5_O2':                 'AIN4',           # oxygen %. actually V * 10
+         '4Average_bottom_P':    'AIN0_EF_READ_A', # average pressure using EF. This will be the the value used
+         'raw4':                 'AIN4',
+         '5_O2':                 'AIN4_EF_READ_A',           # oxygen level is this reading * 10
          'shutoff':              'DIO0', 
          'door':                 'DIO1', 
          'plant':                'DIO2', 
-         'vent alarm':           'DIO5', 
-         'vent valve':           'DIO6', 
+         'vent_alarm':           'DIO5', 
+         'vent_valve':           'DIO6', 
          'fillpoint':            'AIN6', 
-         'fans1':                'DAC1',
-         'ambient':              'TEMPERATURE_AIR_K'}
+         'ambient':              'TEMPERATURE_AIR_K',
+         'fan_0':                'DAC0',
+         'fan_1':                'DAC1'}
 
 
 #######################################################  #####
@@ -53,6 +58,8 @@ def connect2db():
 
 def retry_cnct():   # loop to allow repeatred retries of cnxn. Why not built into connect_2db?
   db_cnxn = 0
+  global cursor
+
   while not db_cnxn:
     try:
       connect2db()
@@ -62,15 +69,15 @@ def retry_cnct():   # loop to allow repeatred retries of cnxn. Why not built int
       print "failed to connect to ",LN2config.db_db,"on", LN2config.db_host, e
       time.sleep(connect_wait_time)
 
-    return cursor
 ############################  #############
 
 
 while 1:
 
-  cursor = retry_cnct()
+  retry_cnct()
 
   write_loop =   time.time() - write_time >  write_interval    
+  read_ok = 1
 
   try:
     handle = ljm.openS("ANY", "ANY", "Biorepository_T7")  
@@ -82,8 +89,6 @@ while 1:
     #print "\nLabjack {} serial # {} found on ip {} at {}\n-----------".format(devicename, info[2], ljm.numberToIP(info[3]), ts )
     print "\nLabjack {}  at {}\n-----------".format(devicename,  ts )
 
-    ljm.eWriteName(handle, 'DAC1', 4.5)
-
     resultset = {}
     for item in sorted(read_channels.keys()):
       address = read_channels[item]
@@ -93,7 +98,7 @@ while 1:
       if 'fillpoint' in item:
         result =  (55.56*result) + 255.37 - 273.15
 
-      elif 'fans1'  in item:
+      elif 'fans'  in item:
         result = result < 3.5
 
       elif 'Average' in item:
@@ -101,22 +106,28 @@ while 1:
 
       elif 'O2' in item:
          result *=  10
+
       elif 'ambient' in item:
          result -= 273.15
+
+      elif 'plant' in item:
+          result = not result # flip state
        
       resultset[item] = result   #accumulate results in a dictionary
 
       print " {:20} {:25} {:6.4f}".format(item, address, result)
-  except:
-    print 'failed to acces labjack'
+  except Exception, e:
+    print 'failed to acces labjack', e
+    read_ok = 0
+
 
 
   #compare_results(resultset, last_resultset)   # look for difference requiring a write
   
-  if write_loop:
-       sql = """INSERT INTO `pressure` ( `analog0`, `analog1`, `O2`, `switch`, `fill_point`, `ambient`, `door`, `LN2_plant`, `fans_status`, `cause`, `timestamp`, `vent_valve`, `vent_alarm`)
+  if write_loop and read_ok:
+       sql = """INSERT INTO `pressure` ( `analog0`, `analog1`, `O2`, `switch`, `fill_point`, `ambient`, `door`, `LN2_plant`, `fan_0`, `fan_1`, `cause`, `timestamp`, `vent_valve`, `vent_alarm`)
              VALUES
-            ( {2Average top P}, {4Average bottom P}, {5_O2}, {shutoff}, {fillpoint}, {ambient}, {door}, {plant}, {fans1}, 'Time', now(), {vent valve}, {vent alarm});""".format(**resultset)  
+            ( {2Average_top_P}, {4Average_bottom_P}, {5_O2}, {shutoff}, {fillpoint}, {ambient}, {door}, {plant}, {fan_0}, {fan_0}, 'Time', now(), {vent_valve}, {vent_alarm});""".format(**resultset)  
   
   
        print '**** writing to db....', 
@@ -131,7 +142,7 @@ while 1:
          # start retrying it
 
          print e, 'retrying cnxn'
-         cursor = retry_cnct()
+         retry_cnct()
 
   
 
@@ -141,5 +152,5 @@ while 1:
   except:
     print 'failed to close labjack'
   cursor.close()
-  time.sleep(5)
+  time.sleep(sleep_time)
 
